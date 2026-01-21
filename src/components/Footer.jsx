01477929c3cd43e0060d40c065043833400dabc6
Nabilkencana22 <mohammad_kencana_33rpl@student.smktelkom-mlg.sch.id>
2026-01-21 13:22:08 +0700

    penghapusan import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Leaf, Mail, Phone, MapPin, Facebook, Linkedin, Twitter, Instagram, CheckCircle, Loader2 } from 'lucide-react';
+import { Leaf, Mail, Phone, MapPin,CheckCircle, Loader2 } from 'lucide-react';
 
 const Footer = () => {
     const [email, setEmail] = useState('');
