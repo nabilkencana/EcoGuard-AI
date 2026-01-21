ac9148a0dd185a0a718e03395d96176df874df30
RiskyNabil213 <nabilrisky390@gmail.com>
2026-01-21 11:20:40 +0700

    mencoba

--- src/components/common/SectionTitle.jsx
+++ src/components/common/SectionTitle.jsx
@@ -41,6 +41,7 @@ const SectionTitle = ({
                     {title}
                 </h2>
             )}
+            
 
             {subtitle && (
                 <h3 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-4">
